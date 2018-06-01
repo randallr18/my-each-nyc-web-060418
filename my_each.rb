48a771b1580233cc462cd167e010b2fff9b4cf4e
Risher Randall <randallr18@mail.wlu.edu>
2018-06-01 01:59:19 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  counter = 0
+  
+  while counter < collection.length
+  yield (collection[counter])
+    puts i
+  end
+  counter += 1
+end
 end
